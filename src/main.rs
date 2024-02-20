ab0092e5ad8df2e78746c17a59aabec8107b160d
Sasha Aldrick <sasha@aldrick.dev>
2024-02-20 15:18:21 +0000

    added proper error handling, let's gooooooooooo

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use serde::Deserialize;
 use serde_json::json;
 use std::time::Instant;
 use tokio;
-use indicatif::{ProgressBar};
+use indicatif::ProgressBar;
 
 #[derive(Deserialize, Debug)]
 struct ChallengeResponse {
@@ -17,15 +17,9 @@ struct ChallengeResponse {
     difficulty: u32,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize)]
 struct VerifyResponse {
-    status: String,
     challenge: String,
-    #[serde(rename = "challengeCounter")]
-    challenge_counter: u32,
-    #[serde(rename = "challengesNeeded")]
-    challenges_needed: u32,
-    difficulty: u32,
 }
 
 #[derive(Deserialize, Debug)]
@@ -37,39 +31,55 @@ struct TxHashResponse {
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> Result<(), Error> {
     let client = reqwest::Client::new();
+  
+    let challenge_response = match challenge_request(&client).await {
+        Ok(response) => response,
+        Err(e) => {
+            println!("The error is {}.", e);
+            return Err(e);
+        }
+    };
 
-    let challenge_response = challenge_request(&client).await?;
-    let challenges_needed: u32 = challenge_response.challenges_needed;
-    let mut counter = challenge_response.challenge_counter;
-    let mut current_challenge_string = challenge_response.challenge;
-    let difficulty = challenge_response.difficulty;
+    let ChallengeResponse {challenge, mut challenge_counter, challenges_needed, difficulty} = challenge_response;
+    let mut current_challenge_string = challenge.clone();
 
-    let pb = ProgressBar::new(challenges_needed as u64);
-
+    // debug: print, progress bar, timing
     println!("Challenges Needed: {}", challenges_needed);
+    let pb = ProgressBar::new(challenges_needed as u64);
     let start_time = Instant::now(); // Start the timer
 
-    while counter <= challenges_needed {
-        // println!("Started solve challenge loop 🫡");
-        // println!("Current Counter: {}", counter);
-        // println!("Challenge {}", counter);
+    // cycle solving challenges
+    while challenge_counter <= challenges_needed {
         let (correct_hash, nonce) = solve_challenge(&current_challenge_string, &difficulty);
 
-        if counter != challenges_needed {
-            let verify_response = verify_request(&client, correct_hash, nonce).await?;
-            // println!("Status of /verify request: {}", verify_response.status);
-            counter += 1;
+        if challenge_counter != challenges_needed {
+            let verify_response = match verify_request(&client, correct_hash, nonce).await {
+                Ok(response) => response,
+                Err(e) => {
+                    println!("The error is {}.", e);
+                    return Err(e);
+                }
+            };
+
             current_challenge_string = verify_response.challenge;
+            challenge_counter += 1;
         } else {
-            // println!("Send manual request using: {}/{}", correct_hash, nonce);
-            // println!("In TxHashResponse Scope!");
-            let tx_hash_response = tx_hash_request(&client, correct_hash, nonce).await?;
-            // println!("Operation Hash: {}", tx_hash_response.tx_hash);
-            open::that(format!("https://ghostnet.tzkt.io/{}/11694578", tx_hash_response.tx_hash));
-            // TODO add error handling here
-            counter += 1;
+            let tx_hash_response = match tx_hash_request(&client, correct_hash, nonce).await {
+                Ok(response) => response,
+                Err(e) => {
+                    println!("The error is {}.", e);
+                    return Err(e);
+                }
+            };
+            match open::that(format!("https://ghostnet.tzkt.io/{}/", tx_hash_response.tx_hash)) {
+                Ok(_) => (),
+                Err(e) => {
+                    println!("Failed to open URL: {}", e);
+                }
+            };
+            challenge_counter += 1;
         }
 
         pb.inc(1);
@@ -80,13 +90,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     pb.finish_with_message("Completed!");
     println!("Cumulative time taken: {:.3} s", duration.as_secs_f64());
 
-    Ok(())
+   Ok(())
 }
 
 async fn challenge_request(client: &reqwest::Client) -> Result<ChallengeResponse, Error> {
     let challenge_post_data = json!({
-        "address": "tz1g8vkmcde6sWKaG2NN9WKzCkDM6Rziq194", // Replace with the actual address you want to use
-        "amount": 1
+        "address": "tz1ZcrFLMV2LkyYpVvL49p5hmBRpoAHf8W4q", // Replace with the actual address you want to use
+        "amount": 10
     });
 
     // Perform the POST request to get the challenge string
@@ -96,10 +106,9 @@ async fn challenge_request(client: &reqwest::Client) -> Result<ChallengeResponse
         .send()
         .await?;
 
-    // Deserialize the response into ChallengeResponse
-    let challenge_response: Result<ChallengeResponse, Error> = res.json().await;
+    // Deserialize the response
+    res.json().await
 
-    challenge_response
 }
 
 async fn verify_request(
@@ -108,8 +117,8 @@ async fn verify_request(
     nonce: u32,
 ) -> Result<VerifyResponse, Error> {
     let verify_post_data = json!({
-        "address": "tz1g8vkmcde6sWKaG2NN9WKzCkDM6Rziq194", // Replace with the actual address you want to use
-        "amount": 1,
+        "address": "tz1ZcrFLMV2LkyYpVvL49p5hmBRpoAHf8W4q", // Replace with the actual address you want to use
+        "amount": 10,
         "nonce": nonce,
         "solution": correct_hash
     });
@@ -122,7 +131,7 @@ async fn verify_request(
         .await?;
 
     // Deserialize the response into ChallengeResponse
-    let verify_response: Result<VerifyResponse, Error> = res.json().await;
+    let verify_response = res.json().await;
 
     verify_response
 }
@@ -133,8 +142,8 @@ async fn tx_hash_request(
     nonce: u32,
 ) -> Result<TxHashResponse, Error> {
     let verify_post_data = json!({
-        "address": "tz1g8vkmcde6sWKaG2NN9WKzCkDM6Rziq194", // Replace with the actual address you want to use
-        "amount": 1,
+        "address": "tz1ZcrFLMV2LkyYpVvL49p5hmBRpoAHf8W4q", // Replace with the actual address you want to use
+        "amount": 10,
         "nonce": nonce,
         "solution": correct_hash
     });
